02db44ea45af81d66c94a9298904d47592ed8eb9
ma569624 <ms569624@gmail.com>
2024-01-05 23:30:47 -0800

    try to fix cor error

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const connectDB = require("./db/connect")
 
 const PORT = process.env.PORT || 5000 ;
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 
 const router = require('./routes/products')
 
