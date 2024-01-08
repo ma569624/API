fa43cf493e9714f837ccd4968311d966619cddcc
ma569624 <ms569624@gmail.com>
2024-01-08 11:43:11 -0800

    u[load image

--- index.js
+++ index.js
@@ -24,11 +24,24 @@ app.use("/api/products", router);
 app.use("/api/home", homerouter);
 
 app.use('/profile', express.static('upload/images'));
+const path = require("path");
 //middleware
 
-// Set up multer to handle form data
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+// storage engine 
+
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req, file, cb) => {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 10000
+    }
+})
 
 app.post('/api/products', upload.single('image'), (req, res) => {
     // Access form fields and uploaded files using req.body and req.file
@@ -40,7 +53,7 @@ app.post('/api/products', upload.single('image'), (req, res) => {
     // For demonstration purposes, we'll just log the details
     console.log('Product Name:', productName);
     console.log('Product Price:', productPrice);
-    console.log('Product Image Size:', productImage.length);
+    // console.log('Product Image Size:', productImage.length);
   
     // Respond with a success message
     res.json({ message: 'Product created successfully' });
