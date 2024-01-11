0607089b818c249f8873200e46aacace3957622e
Manish S <72225842+ma569624@users.noreply.github.com>
2024-01-11 11:56:17 +0530

    Update homeroute.js

--- routes/homeroute.js
+++ routes/homeroute.js
@@ -4,22 +4,7 @@ const homerouter = express.Router();
 const {postHomeBanner , getHomeBanner, EditHomeBanner, DeleteHomeBanner} = require('../controllers/home');
 const multer = require("multer");
 const path = require("path");
-
-// storage engine 
-
-const storage = multer.diskStorage({
-    destination: './upload/images',
-    filename: (req, file, cb) => {
-        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
-    }
-})
-
-const upload = multer({
-    storage: storage,
-    limits: {
-        fileSize: 100000
-    }
-})
+const upload = require("../middleware/uploader");
 
 
 homerouter.route('/banner').post(upload.single('profile'), postHomeBanner);
